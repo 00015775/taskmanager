cb9e7ac6663d241ee75076972ce11e5979005fde
00015775 <00015775WIUT@gmail.com>
2026-02-22 16:21:41 +0500

    feat: add Task, Tag, UserProfile models with relationships

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Task, Tag
 
-# Register your models here.
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'color']
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['title', 'owner', 'priority', 'status', 'due_date', 'created_at']
+    list_filter = ['priority', 'status', 'tags']
+    search_fields = ['title', 'owner__username']
+    filter_horizontal = ['tags']
