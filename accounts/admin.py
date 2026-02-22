cae1931e13944ca2bb7216a530ed8732c5c65ca3
00015775 <00015775WIUT@gmail.com>
2026-02-22 19:27:36 +0500

    chore: configure admin panel, static files, and CSS styling

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import UserProfile
 
+
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'created_at']
+    list_display = ['user', 'user_email', 'created_at']
     search_fields = ['user__username', 'user__email']
+    readonly_fields = ['created_at']
+
+    def user_email(self, obj):
+        return obj.user.email
+    user_email.short_description = 'Email'
